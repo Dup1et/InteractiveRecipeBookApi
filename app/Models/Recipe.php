ff26b680964ac31e1124edeeb9764621fbf9640a
Dup1et <none@gmail.com>
2020-11-21 21:31:08 +0300

    feat: add relations to Recipe

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -21,4 +21,16 @@ class Recipe extends Model
         'recipe_body_id',
         'user_id',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function language()
+    {
+        return $this->belongsTo(Language::class, 'language_id');
+    }
+
+    protected $dates = ['created_at', 'updated_at'];
 }
