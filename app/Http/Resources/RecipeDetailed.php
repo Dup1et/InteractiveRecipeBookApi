02f660b82df90660a83c9030ecdbf1018d5598b9
Dup1et <none@gmail.com>
2021-01-02 21:13:54 +0300

    feat: add detailed resource for recipes

---
+++ app/Http/Resources/RecipeDetailed.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RecipeDetailed extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'preview' => $this->preview,
+            'cookingTime' => $this->cooking_time,
+            'portions' => $this->portions,
+            'language' => new Language($this->language),
+            'body' => json_encode($this->body),
+            'user' => new User($this->user),
+            'publishedAt' => $this->created_at->format('d.m.Y H:i:s'),
+        ];
+    }
+}
